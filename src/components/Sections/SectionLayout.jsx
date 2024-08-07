96e371aa380aef29d17999fedc06f7ab7d6f66a4
Elisama Carvalho <elisamalinscarvalho@hotmail.com>
2024-08-06 23:21:35 -0300

    feat: cria componente base e de layout para secoes

--- src/components/Sections/SectionLayout.jsx
+++ src/components/Sections/SectionLayout.jsx
@@ -1,9 +1,19 @@
-const SectionLayout = ({children}) => {
+import PropTypes from 'prop-types';
+
+const SectionLayout = ({children, background}) => {
   return (
-    <section className="p-24 px-8 tablet:p-16 tablet:px-7 xs:p-12 xs:px-7 light-gray">
+  <section className={`p-24 px-8 tablet:p-16 tablet:px-7 xs:p-12 xs:px-7 ${background}`}>
       {children}
     </section>
 );
+}
+
+SectionLayout.defaultProps = {
+  background: "light-gray"
+}
+
+SectionLayout.propTypes = {
+  background: PropTypes.string
 }
 
 export default SectionLayout;
--- src/components/Sections/index.jsx
+++ src/components/Sections/index.jsx
@@ -1,8 +1,14 @@
+import SectionEBD from "./SectionEBD";
 import SectionMinistries from "./SectionMinistries";
+import SectionLocationAndSchedule from "./SectionLocationAndSchedule";
 
 const Sections = () => {
     return (
+      <>
       <SectionMinistries />
+      <SectionEBD />
+      <SectionLocationAndSchedule />
+      </>
   )
 }
 
