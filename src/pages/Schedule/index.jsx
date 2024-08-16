8b7a66448ddbdfa45f0006f600d077231aca1f8e
Elisama Carvalho <elisamalinscarvalho@hotmail.com>
2024-08-15 21:57:07 -0300

    feat: adiciona informacoes para pagina de agenda

--- src/pages/Schedule/index.jsx
+++ src/pages/Schedule/index.jsx
@@ -2,21 +2,49 @@ import SectionLayout from '../../components/Sections/SectionLayout';
 import SectionTitle from '../../components/Sections/SectionTitle';
 
 const Schedule = () => {
+  const schedule = [
+    {
+      title: "Terça",
+      description: "7:30h | Culto de Consagração"
+    }, 
+    {
+      title: "Quarta",
+      description: "19h | Culto de Círculo de Oração"
+    }, 
+    {
+      title: "Sexta",
+      description: "19:30h | Aviva Joshua"
+    }, 
+    {
+      title: "Domingo",
+      description: "18h | Culto da Família"
+    }
+  ]
   return (
     <>
-    <div>
+    <div className="flex justify-center light-gray">
       <SectionLayout>
       <SectionTitle
         title={
-          <p>
-            Agenda
+          <p className="flex justify-center">
+            Agenda da semana
           </p>
         }
+        manualDivider
       />
-      <div className="flex flex-row">
-        <p className="justify-start max-w-96 pt-10 pl-12">
-          Igreja Batista Joshua
-        </p>
+      <div className="flex flex-col text-center mt-8 p-5 justify-center max-w-4xl beige-orange rounded-lg">
+        {
+          schedule.map((item) => (
+            <>
+            <p className="text-white pt-4 font-semibold">
+              {item.title}
+            </p>
+            <p className="text-white">
+              {item.description}
+            </p>
+            </>
+          ))
+        }
       </div>
       </SectionLayout>
     </div>
